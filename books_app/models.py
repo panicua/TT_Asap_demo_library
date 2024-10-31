4ef4e450326d8f1d33c9b7302b921b6b070086e7
kekW <panicua12@gmail.com>
2024-10-31 20:30:59 +0200

    ref: pages (positive int), isbn+language (null=False)

--- books_app/models.py
+++ books_app/models.py
@@ -5,10 +5,12 @@ class Book(models.Model):
     title = models.CharField(max_length=255, blank=False)
     author = models.CharField(max_length=255, blank=False)
     published_date = models.DateField(blank=True, null=True)
-    isbn = models.CharField(max_length=20, unique=True, blank=False)
-    pages = models.IntegerField(blank=True, null=True)
+    isbn = models.CharField(
+        max_length=20, unique=True, blank=False, null=False
+    )
+    pages = models.PositiveIntegerField(blank=True, null=True)
     cover = models.URLField(blank=True, null=True)
-    language = models.CharField(max_length=50, blank=False)
+    language = models.CharField(max_length=50, blank=False, null=False)
 
     class Meta:
         ordering = ["title"]
