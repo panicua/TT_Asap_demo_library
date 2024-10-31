82db60b88cfd4492a7622aff76bd0101d0c4acd0
kekW <panicua12@gmail.com>
2024-10-31 02:46:35 +0200

    add: Book model to books_app

--- books_app/models.py
+++ books_app/models.py
@@ -1,3 +1,14 @@
 from django.db import models
 
-# Create your models here.
+
+class Book(models.Model):
+    title = models.CharField(max_length=255, blank=False)
+    author = models.CharField(max_length=255, blank=False)
+    published_date = models.DateField(blank=True, null=True)
+    isbn = models.CharField(max_length=20, unique=True, blank=False)
+    pages = models.IntegerField(blank=True, null=True)
+    cover = models.URLField(blank=True, null=True)
+    language = models.CharField(max_length=50, blank=False)
+
+    def __str__(self):
+        return f"{self.title} (by {self.author})"
