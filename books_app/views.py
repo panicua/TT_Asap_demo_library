354667b07b73984638935653f20d654ebf4a8240
kekW <panicua12@gmail.com>
2024-10-31 20:36:44 +0200

    add: BookViewSet full CRUD operations

--- books_app/views.py
+++ books_app/views.py
@@ -1,3 +1,18 @@
-from django.shortcuts import render
+from rest_framework import viewsets
 
-# Create your views here.
+from books_app.models import Book
+from books_app.permissions import IsAdminOrReadOnly
+from books_app.serializers import BookSerializer
+from books_app.pagination import BookListPagination
+from books_app.filters import BookFilter
+
+from django_filters.rest_framework import DjangoFilterBackend
+
+
+class BookViewSet(viewsets.ModelViewSet):
+    queryset = Book.objects.all()
+    serializer_class = BookSerializer
+    pagination_class = BookListPagination
+    permission_classes = [IsAdminOrReadOnly]
+    filter_backends = [DjangoFilterBackend]
+    filterset_class = BookFilter
